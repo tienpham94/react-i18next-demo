5b941c8ddcf8d085add0429ad630a01f104d97e5
Tien Pham <tiendev94@gmail.com>
2019-09-22 19:35:46 +0300

    Add i18next and change language

---
+++ src/HeaderPart.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Header, Button } from 'semantic-ui-react';
+import { useTranslation} from 'react-i18next';
+ 
+const HeaderPart = () => {
+ const { t, i18n } = useTranslation("messages");
+ const changeLanguage = lng => {
+   i18n.changeLanguage(lng);
+ };
+ 
+ return(<Header as='h2' block color="teal" textAlign="center">
+   {t("title")}
+   <Button.Group floated='right'>
+   <Button onClick={() => changeLanguage('en')}>English</Button>
+    <Button.Or />
+   <Button onClick={() => changeLanguage('de')}>German</Button>
+ </Button.Group>
+ </Header>
+);
+}
+ 
+export default HeaderPart;
